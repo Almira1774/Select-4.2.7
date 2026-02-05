d16b077119b8cb68ecf387cb3ac3380b2da7cc4c
Almira Vasiluk <bezzbashennaya@gmail.com>
2026-02-05 15:51:26 +0300

    переименовала методы и переменные

--- xhr.js
+++ xhr.js
@@ -4,23 +4,21 @@ class View {
         this.searchWrapper = this.createElement('div', 'search-wrapper');
 
         this.body.append(this.searchWrapper);
-        this.searchLine = this.createElement('div', 'search-line');
+        this.inputWrapper = this.createElement('div', 'input-wrapper');
 
-        this.searchInput = this.createElement('input', 'search-input');
+        this.searchInput = this.createElement('input', 'input-wrapper_input');
         this.searchInput.type = 'text';
         this.searchInput.placeholder = 'Введите имя реппозитория...';
 
-        this.usersWrapper = this.createElement('div', 'users-wrapper');
-        this.usersList = this.createElement('ul', 'users-list');
+        this.repositoriesWrapper = this.createElement('div', 'repositories-wrapper');
+        this.repositoriesList = this.createElement('ul', 'repositories-wrapper__list');
 
-        this.usersWrapper.append(this.usersList);
-        this.searchLine.append(this.searchInput);
-        this.searchLine.append(this.usersWrapper);
-        this.searchWrapper.append(this.searchLine);
-
-
-
+        this.repositoriesWrapper.append(this.repositoriesList);
+        this.inputWrapper.append(this.searchInput);
+        this.inputWrapper.append(this.repositoriesWrapper);
+        this.searchWrapper.append(this.inputWrapper);
     }
+
     createElement(elementTagNAme, elementClassName) {
         const element = document.createElement(elementTagNAme);
         if (elementClassName) {
@@ -30,19 +28,16 @@ class View {
     }
     createUser(userData, className) {
 
-
-
-
         // Создаем элемент пользователя
         const userElement = this.createElement('li', className);
-        userElement.innerHTML = `<div class="user-info" > 
-                                   <span class="user-name">Name: ${userData.name}</span><br>
-                                   <span class="user-owner">Owner: ${userData.owner}</span><br>
+        userElement.innerHTML = `<div class="repository-info" > 
+                                   <span class="repository-info_name">Name: ${userData.name}</span><br>
+                                   <span class="repository-info_owner">Owner: ${userData.owner}</span><br>
                                    <span>Stars: ${userData.stars}</span> 
                                  </div>`;
 
 
-        this.usersList.append(userElement);
+        this.repositoriesList.append(userElement);
 
         return userElement;
     }
@@ -59,8 +54,8 @@ class View {
         })
     }
 
-    searchClear() {
-        this.usersList.innerHTML = ''; //очищаем список поиска
+    clearSearchList() {
+        this.repositoriesList.innerHTML = ''; //очищаем список поиска
     }
     createData(user) {
         return {
@@ -89,20 +84,25 @@ class Search {
     constructor(view) {
         this.view = view;
         this.controller = new AbortController();
-
         this.debouncedSearchUsers = this.view.debounce(this.searchUsers.bind(this), 600);
-
         this.view.searchInput.addEventListener('keyup', this.debouncedSearchUsers.bind(this));
         this.view.searchInput.addEventListener('keypress', this.handleKeyPress.bind(this));
         this.addedIds = new Set();
+        
+        this.view.searchWrapper.addEventListener('click', (event) => {
+            if (!this.view.searchInput.contains(event.target)) {
+                console.log('clicked outside');
+                this.view.clearSearchList();
+                this.view.searchInput.value = '';
+            }
+        })
 
     }
-    createCheckedList(user) {
 
-        const checkedItem = this.view.createElement('div', 'added-users')
-        const addedElement = this.view.createUser(user, 'added-users-item')
+    createaddedList(user) {
 
-        console.log(addedElement)
+        const repositoryWraper = this.view.createElement('div', 'added-repository-wraper')
+        const addedElement = this.view.createUser(user, 'added-repository')
         const btnClose = this.view.createElement('div', 'btn-close')
         btnClose.innerHTML = `
     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -110,21 +110,21 @@ class Search {
     </svg>
 `;
         btnClose.addEventListener('click', () => {
-            checkedList.removeChild(checkedItem)
+            addedList.removeChild(repositoryWraper)
             this.addedIds.delete(user.id);
             console.log(this.addedIds)
 
         })
-        let checkedList = this.view.searchWrapper.querySelector('.checked-users-wrapper')
-        if (!checkedList) {
-            checkedList = this.view.createElement('div', 'checked-users-wrapper')
-            this.view.searchWrapper.append(checkedList)
+        let addedList = this.view.searchWrapper.querySelector('.added-list')
+        if (!addedList) {
+            addedList = this.view.createElement('div', 'added-list')
+            this.view.searchWrapper.append(addedList)
         }
-        this.view.onFocuse(checkedItem);
-        checkedItem.append(addedElement)
-        checkedItem.append(btnClose)
-        checkedList.append(checkedItem)
-        this.view.searchClear()
+        this.view.onFocuse(repositoryWraper);
+        repositoryWraper.append(addedElement)
+        repositoryWraper.append(btnClose)
+        addedList.append(repositoryWraper)
+        this.view.clearSearchList()
         this.view.searchInput.value = ''
         return addedElement
     }
@@ -146,23 +146,13 @@ class Search {
 
         if (this.view.searchInput.value.trim() === '') {
             this.abortRequest();
-            this.view.searchClear();
+            this.view.clearSearchList();
             return;
         }
 
-        
+        try {         
 
-        try {
-            
-            this.view.searchWrapper.addEventListener('click', (event) => {
-                if (!this.view.searchInput.contains(event.target)) {
-                    console.log('clicked outside');
-                    this.view.searchClear();
-                    this.view.searchInput.value = '';
-                }
-            })
-         
-       
+
             const signal = this.controller.signal;
             const response = await fetch(`https://api.github.com/search/repositories?q=${this.view.searchInput.value}`, {
                 signal: signal,
@@ -170,33 +160,34 @@ class Search {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
+            
             const data = await response.json();
-            this.view.searchClear();
+            if (!data.items || data.items.length === 0) {
+                throw new Error(`No data found`);
+            }
+            this.view.clearSearchList();
 
             const result = data.items.slice(0, 5);
 
             result.forEach(item => {
 
                 const userData = this.view.createData(item)
-
-
                 const userItem = this.view.createUser(userData, 'user');
-
                 this.view.onFocuse(userItem);
                 console.log(userItem)
 
                 userItem.addEventListener('click', () => {
                     if (this.addedIds.has(userData.id)) {
                         console.log('This user is already added!');
-                        this.view.searchClear();
+                        this.view.clearSearchList();
                         this.view.searchInput.value = '';
                         return;
                     }
-                    this.createCheckedList(userData);
+                    this.createaddedList(userData);
                     console.log(userData.id)
                     this.addedIds.add(userData.id);
                     console.log(this.addedIds)
-                   
+
                 })
 
             })
